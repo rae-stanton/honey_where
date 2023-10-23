e154e6541015200b0c43c30b9c8c187737f84de2
Rae Stanton <raestanton93@gmail.com>
2023-10-23 14:21:09 -0700

    Added ability to migrate and started basic table for testing

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://raestanton@localhost/honey_where'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)  # bind db from models.py to Flask app
+migrate = Migrate(app, db)
 
 @app.route("/")
 def hello():
---
+++ backend/models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, primary_key=True, nullable=False)
+
+    def __repr__(self):
+        return f'(id={self.id}, name={self.name})'
