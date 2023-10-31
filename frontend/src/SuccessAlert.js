8bc174ef885e9582e3fbf9a4cb37d5a11c3b96e5
Rae Stanton <raestanton93@gmail.com>
2023-10-31 13:48:00 -0700

    light form styling for adding multiple rooms

--- frontend/src/SuccessAlert.js
+++ frontend/src/SuccessAlert.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function DismissibleSuccessAlert() {
   const [show, setShow] = useState(true);
+  const navigate = useNavigate();
 
   if (!show) return null;
 
   return (
     <Alert variant="success" onClose={() => setShow(false)} dismissible>
-      <Alert.Heading>Your home has been added successfully!</Alert.Heading>
+      <Alert.Heading>Added successfully!</Alert.Heading>
       <p>
-        To view your home, click here
+        To view your home, <span style={{color: 'blue', cursor: 'pointer'}} onClick={() => navigate("/")}>click here</span>.
       </p>
     </Alert>
   );
