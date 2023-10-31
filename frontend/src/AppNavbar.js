90cab88cd1140e3f858c6d6392eab015c0a3ff29
Rae Stanton <raestanton93@gmail.com>
2023-10-31 14:20:30 -0700

    Adds nav items if logged in

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -5,24 +5,38 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./AppNavbar.css";
 
-function AppNavbar({isLoggedIn}) {
-  console.log(isLoggedIn)
+function AppNavbar({ isLoggedIn }) {
+  console.log(isLoggedIn);
   return (
     <Navbar className="main-navbar" expand="lg">
       <Container className="d-flex justify-content-between">
         {/* You can uncomment and add back the logo or any other navbar components here if needed */}
         <Nav className="flex-grow-1 justify-content-end align-items-center">
-          <Nav.Link as={Link} to="/" className="navlink">Home</Nav.Link>
-          {
-            isLoggedIn ? (
-              <Nav.Link as={Link} to="/logout" className="navlink">Logout</Nav.Link>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/signup" className="navlink">Sign Up</Nav.Link>
-                <Nav.Link as={Link} to="/login" className="navlink">Log In</Nav.Link>
-              </>
-            )
-          }
+          <Nav.Link as={Link} to="/" className="navlink">
+            Home
+          </Nav.Link>
+          {isLoggedIn ? (
+            <>
+              <Nav.Link as={Link} to="/add-home" className="navlink">
+                Add Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/add-room" className="navlink">
+                Add Rooms
+              </Nav.Link>
+              <Nav.Link as={Link} to="/logout" className="navlink">
+                Logout
+              </Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link as={Link} to="/signup" className="navlink">
+                Sign Up
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login" className="navlink">
+                Log In
+              </Nav.Link>
+            </>
+          )}
         </Nav>
       </Container>
     </Navbar>
