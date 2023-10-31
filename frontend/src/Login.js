4fe1a1f6bb9ad13986c15f9e907c0400d271288d
Rae Stanton <raestanton93@gmail.com>
2023-10-31 14:12:14 -0700

    alert from redirect

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,104 +1,139 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Formik, Field, Form } from "formik";
-import { Card, Button } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Card, Button, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
-import "./LoginStyling.css"
-import "bootstrap/dist/css/bootstrap.min.css"; // Don't forget to import Bootstrap CSS
+import "./LoginStyling.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Login({ setIsLoggedIn, setUserName }) {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const [showAlert, setShowAlert] = useState(
+    location.state?.fromPrivateRoute || false
+  );
+
+  useEffect(() => {
+    if (showAlert) {
+      const timeout = setTimeout(() => {
+        setShowAlert(false);
+      }, 5000); // 5 seconds
+
+      // Cleanup to avoid memory leaks if component is unmounted
+      return () => clearTimeout(timeout);
+    }
+  }, [showAlert]);
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-85">
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        onSubmit={async (values, { setSubmitting }) => {
-          try {
-            const response = await axios.post("http://127.0.0.1:5000/login", {
-              email: values.email,
-              password: values.password,
-            });
+    <>
+      {showAlert && (
+        <div className="d-flex justify-content-center mt-5">
+          <Alert
+            variant="danger"
+            onClose={() => setShowAlert(false)}
+            dismissible
+          >
+            Uh-oh! You have to be logged in to do that.
+          </Alert>
+        </div>
+      )}
+      <div className="d-flex justify-content-center align-items-center vh-85">
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={async (values, { setSubmitting }) => {
+            try {
+              const response = await axios.post("http://127.0.0.1:5000/login", {
+                email: values.email,
+                password: values.password,
+              });
 
-            if (response.status === 200) {
-              localStorage.setItem("access_token", response.data.access_token);
-              localStorage.setItem(
-                "refresh_token",
-                response.data.refresh_token
-              );
-              localStorage.setItem("user_name", response.data.user_name);
-              setIsLoggedIn(true);
-              setUserName(response.data.user_name);
-              navigate("/"); // Redirect to home
-            } else {
-              alert("Login failed");
+              if (response.status === 200) {
+                localStorage.setItem(
+                  "access_token",
+                  response.data.access_token
+                );
+                localStorage.setItem(
+                  "refresh_token",
+                  response.data.refresh_token
+                );
+                localStorage.setItem("user_name", response.data.user_name);
+                setIsLoggedIn(true);
+                setUserName(response.data.user_name);
+                navigate("/"); // Redirect to home
+              } else {
+                alert("Login failed");
+              }
+            } catch (error) {
+              console.error("Login error", error);
+              alert("Login error");
+            } finally {
+              setSubmitting(false);
             }
-          } catch (error) {
-            console.error("Login error", error);
-            alert("Login error");
-          } finally {
-            setSubmitting(false);
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Card className="mt-5 w-80 forms">
-            <Card.Body>
-              <h4 className="header-text text-center text-primary mb-4">Login</h4>
-              <Form>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                  <Field
-                    as="input"
-                    id="email"
-                    name="email"
-                    placeholder="Your email"
-                    type="email"
-                    className="form-control"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">
-                    Password
-                  </label>
-                  <Field
-                    as="input"
-                    id="password"
-                    name="password"
-                    placeholder="Your password"
-                    type="password"
-                    className="form-control"
-                  />
-                </div>
-                <Button
-                  type="submit"
-                  className="w-100 mb-3 login-button"
-                  disabled={isSubmitting}
-                >
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Card className="mt-5 w-80 forms">
+              <Card.Body>
+                <h4 className="header-text text-center text-primary mb-4">
                   Login
-                </Button>
-              </Form>
-              <p className="text-center p-text-login">
-                Don't have an account?
-                <Nav.Link
-                  as={Link}
-                  to="/signup"
-                  className="text-primary ms-2 d-inline-block"
-                >
-                  Sign Up
-                </Nav.Link>
-              </p>
-            </Card.Body>
-          </Card>
-        )}
-      </Formik>
-    </div>
+                </h4>
+                <Form>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                    <Field
+                      as="input"
+                      id="email"
+                      name="email"
+                      placeholder="Your email"
+                      type="email"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <Field
+                      as="input"
+                      id="password"
+                      name="password"
+                      placeholder="Your password"
+                      type="password"
+                      className="form-control"
+                    />
+                  </div>
+                  <Button
+                    type="submit"
+                    className="w-100 mb-3 login-button"
+                    disabled={isSubmitting}
+                  >
+                    Login
+                  </Button>
+                </Form>
+                <p className="text-center p-text-login">
+                  Don't have an account?
+                  <Nav.Link
+                    as={Link}
+                    to="/signup"
+                    className="text-primary ms-2 d-inline-block"
+                  >
+                    Sign Up
+                  </Nav.Link>
+                </p>
+              </Card.Body>
+            </Card>
+          )}
+        </Formik>
+      </div>
+    </>
   );
 }
 
