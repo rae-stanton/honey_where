a46e56f992cb66b2067acb37cdd3c9fbfb9a6995
Rae Stanton <raestanton93@gmail.com>
2023-11-01 15:09:13 -0700

    ugly but functional search
    rough starting search

--- frontend/src/UserDash.js
+++ frontend/src/UserDash.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Dropdown from "react-bootstrap/Dropdown";
 
-function UserDash({userName}) {
+function UserDash({ userName }) {
   const token = localStorage.getItem("access_token");
   const [userData, setUserData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedItemType, setSelectedItemType] = useState("");
 
   useEffect(() => {
     const fetchUserDetails = async () => {
-      // Fetch user id from local storage.
       const userId = localStorage.getItem("user_id");
 
       if (!userId) return;
-      // Note: the user's id is set on login, so this should just redirect if nil/Null
+
       try {
         const response = await axios.get(
           `http://127.0.0.1:5000/user_home_details`,
           {
             headers: {
-              'Authorization': `Bearer ${token}`
-            }
+              Authorization: `Bearer ${token}`,
+            },
           }
         );
         setUserData(response.data);
@@ -31,54 +33,93 @@ function UserDash({userName}) {
     fetchUserDetails();
   }, []);
 
+  const filterItemsByName = (item) => {
+    return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const filterItemsByType = (item) => {
+    if (selectedItemType && selectedItemType !== "") {
+      return item.item_type === selectedItemType;
+    }
+    return true;
+  };
+
   if (!userData) return <p>Loading user details...</p>;
 
   return (
     <div className="dashboard">
       <h2>Welcome back, {userName}</h2>
 
+      <div className="filter-section">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <br />
+        <Dropdown onSelect={(selectedKey) => setSelectedItemType(selectedKey)}>
+          <Dropdown.Toggle
+            variant="success"
+            id="dropdown-basic"
+            style={{ padding: "10px" }}
+          >
+            Filter by Type
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="">None</Dropdown.Item>
+            {Object.values(ItemType).map((type) => (
+              <Dropdown.Item eventKey={type} key={type}>
+                {type}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
       {userData.home && (
         <div className="home-details">
           <h3>Your Home: {userData.home.name}</h3>
           {userData.home.rooms && userData.home.rooms.length > 0 ? (
             <ul>
-            <h3>Your rooms:</h3>
+              <h3>Your rooms:</h3>
               {userData.home.rooms.map((room) => (
                 <li key={room.id}>
                   {room.name} ({room.room_type})
                   {/* Display items of the room */}
-                  {room.items && room.items.length > 0 && (
-                    <ul>
-                    <h5>Your items in the main room:</h5>
-                      {room.items.map((item) => (
-                        <li key={item.id}>
-                          {item.name} ({item.item_type})
-                        </li>
+                  <h5>Your items in this room:</h5>
+                  {room.items &&
+                    room.items
+                      .filter(filterItemsByName)
+                      .filter(filterItemsByType)
+                      .map((item) => (
+                        <ul>
+                          <li key={item.id}>
+                            {item.name} ({item.item_type})
+                          </li>
+                        </ul>
                       ))}
-                    </ul>
-                  )}
                   {/* Display subrooms of the room */}
-                  {room.subrooms && room.subrooms.length > 0 && (
-                    <ul>
-                    <h4>Your subrooms:</h4>
-                      {room.subrooms.map((subroom) => (
-                        <li key={subroom.id}>
-                          {subroom.name}
-                          {/* Display items of the subroom */}
-                          {subroom.items && subroom.items.length > 0 && (
-                            <ul>
-                            <h4>Your items in subrooms:</h4>
-                              {subroom.items.map((item) => (
+                  {room.subrooms &&
+                    room.subrooms.map((subroom) => (
+                      <li key={subroom.id}>
+                        {subroom.name}
+                        {/* Display items of the subroom */}
+                        {subroom.items &&
+                          subroom.items
+                            .filter(filterItemsByName)
+                            .filter(filterItemsByType)
+                            .map((item) => (
+                              <ul>
+                                <h4>Your items in subrooms:</h4>
                                 <li key={item.id}>
                                   {item.name} ({item.item_type})
                                 </li>
-                              ))}
-                            </ul>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
+                              </ul>
+                            ))}
+                      </li>
+                    ))}
                 </li>
               ))}
             </ul>
@@ -91,5 +132,18 @@ function UserDash({userName}) {
   );
 }
 
+const ItemType = {
+  HOUSEHOLD: "HOUSEHOLD",
+  DECORATION: "DECORATION",
+  TOOL: "TOOL",
+  UTENSIL: "UTENSIL",
+  APPLIANCE: "APPLIANCE",
+  PHOTO: "PHOTO",
+  PERSONAL: "PERSONAL",
+  ELECTRONIC: "ELECTRONIC",
+  CLOTHING: "CLOTHING",
+  PET: "PET",
+  MISCELLANEOUS: "MISCELLANEOUS",
+};
 
 export default UserDash;
